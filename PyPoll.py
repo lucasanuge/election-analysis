1e3f2f07c3792d1bc22739eb523548a061fdabde
Lucas Chandra <lucas.chandra@outlook.com>
2021-05-14 11:25:27 -0400

    found all candidates and total votes

--- PyPoll.py
+++ PyPoll.py
@@ -2,9 +2,12 @@
 import csv
 import os
 # Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#Initialize Total vote counter
+total_vote = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -13,3 +16,18 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+
+    for row in election_data:
+        #add total vote count.
+        total_vote += 1
+
+        #find all different candidates
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+#show total votes for election
+print(total_vote)
